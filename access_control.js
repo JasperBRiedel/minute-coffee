002f4723af558f6bdba350c50c8f12b5e9850bc6
Jasper Riedel-Rutterford <jasper.riedel-rutterford@tafeqld.edu.au>
2023-10-09 12:58:51 +1000

    simplify access role check

--- access_control.js
+++ access_control.js
@@ -1,11 +1,7 @@
 export default function access_control(allowedRoles) {
   return function (request, response, next) {
     if (request.session.user != null) {
-      if (
-        allowedRoles.some(
-          (allowedRole) => allowedRole === request.session.user.accessRole
-        )
-      ) {
+      if (allowedRoles.includes(request.session.user.accessRole)) {
         next();
       } else {
         response.render("status.ejs", {
