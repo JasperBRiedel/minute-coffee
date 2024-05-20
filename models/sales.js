d883e48c05c620ca3f40826bb9afcb48dc7cf356
Jasper Riedel <jasper.riedelrutterford@tafeqld.edu.au>
2024-05-21 09:53:43 +1000

    Remove unused getAllSalesThisWeek() sales model function to avoid confusion. SalesProducts.getByDateRange() should be used instead.

--- models/sales.js
+++ models/sales.js
@@ -47,33 +47,3 @@ export function create(sale) {
 export function deleteById(saleId) {
     return db_conn.query("DELETE FROM sales WHERE sale_id = ?", [saleId])
 }
-
-
-export function getAllSalesThisWeek() {
-    return db_conn.query(
-        `
-        SELECT 
-            product_name, 
-            product_description, 
-            product_stock,
-            product_price,
-            sale_percentage_off, 
-            (product_price * (1 - sale_percentage_off)) as sale_price, 
-            (DAYOFWEEK(sale_date) - 1) as sale_day, 
-            sale_start_time, 
-            sale_end_time 
-        FROM sales 
-        INNER JOIN products 
-        ON sales.product_id = products.product_id 
-        WHERE sale_date BETWEEN NOW() AND (NOW() + INTERVAL 7 DAY) 
-        AND product_removed = 0
-        `,
-        // DAYOFWEEK returns the day of the date as an integer
-        // (Sunday returns 0, ..., Saturday returns 6)
-        // It seems to always be ahead by one day, hence the - 1
-    
-        // WHERE sale_date BETWEEN NOW() and (NOW() + INTERVAL 7 DAY)
-        // returns only the rows where sale_date is between now and 
-        // 7 days away.
-    )
-}
