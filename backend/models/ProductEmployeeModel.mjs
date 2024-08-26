6e18d030734b663a0d97445d029eb0a303cf0eed
Jasper Riedel <jasper.riedelrutterford@tafeqld.edu.au>
2024-08-26 16:57:10 +1000

    Added updated product employee model

---
+++ backend/models/ProductEmployeeModel.mjs
@@ -0,0 +1,36 @@
+import { DatabaseModel } from "./DatabaseModel.mjs";
+import { EmployeeModel } from "./EmployeeModel.mjs";
+import { ProductModel } from "./ProductModel.mjs";
+
+export class ProductEmployeeModel extends DatabaseModel {
+
+    //// Instance
+
+    constructor(product, employee) {
+        super()
+        this.product = product
+        this.employee = employee
+    }
+
+    //// Static 
+
+    static tableToModel(row) {
+        return new ProductEmployeeModel(
+            ProductModel.tableToModel(row.products),
+            EmployeeModel.tableToModel(row.employees)
+        )
+    }
+
+    /**
+     * 
+     * @returns {Promise<Array<ProductEmployeeModel>>}
+     */
+    static getAll() {
+        return this.query(`
+        SELECT * FROM products INNER JOIN employees 
+        ON products.updated_by_employee_id = employees.id
+        WHERE deleted = 0
+        `, [])
+            .then(result => result.map(row => this.tableToModel(row)))
+    }
+}
