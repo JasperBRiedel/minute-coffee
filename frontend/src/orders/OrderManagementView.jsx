ee87524445649725a95eb577a49bfc27d286202d
Jasper Riedel <jasper.riedelrutterford@tafeqld.edu.au>
2025-05-06 17:37:55 +1000

    Add order management view loading spinner

--- frontend/src/orders/OrderManagementView.jsx
+++ frontend/src/orders/OrderManagementView.jsx
@@ -4,17 +4,15 @@ import XMLDownloadButton from "../common/XMLDownloadButton"
 export function OrderManagementView() {
     const { user, status } = useAuthenticate(["admin"])
 
-    return <section>
-        <XMLDownloadButton 
-        route="/orders/xml"
-        filename="orders.xml"
-        authenticationKey={user && user.authenticationKey}
-        className="btn btn-warning">Export Orders</XMLDownloadButton>
-        order management
-        {status}
-        <p>
-            {JSON.stringify(user)}
-        </p>
+    return <section className="flex flex-col items-center gap-4 p-4">
+        {user
+            ? <XMLDownloadButton
+                route="/orders/xml"
+                filename="orders.xml"
+                authenticationKey={user && user.authenticationKey}
+                className="btn btn-warning">Export Orders</XMLDownloadButton>
+            : <span className="loading loading-spinner loading-xl"></span>
+        }
     </section>
 }
 
