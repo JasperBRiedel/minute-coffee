cb9ecba24468c08526979ab500888a5886886ccb
Jasper Riedel <jasper.riedelrutterford@tafeqld.edu.au>
2025-05-06 10:40:47 +1000

    Added order creation API endpoint

--- backend/controllers/api/APIOrderController.mjs
+++ backend/controllers/api/APIOrderController.mjs
@@ -1,16 +1,70 @@
 import express from "express"
 import { APIAuthenticationController } from "./APIAuthenticationController.mjs"
 import { OrderProductModel } from "../../models/OrderProductModel.mjs"
+import { ORDER_STATUS_PENDING, OrderModel } from "../../models/OrderModel.mjs"
 
 export class APIOrderController {
     static routes = express.Router()
     
     static {
+        this.routes.post("/", this.createOrder)
         this.routes.get(
             "/xml",
             APIAuthenticationController.restrict("admin"),
             this.getOrdersXML
         )
+    }
+
+    /**
+     * Handle creating a new order
+     * 
+     * @type {express.RequestHandler}
+     * @openapi
+     * /api/orders:
+     *      post:
+     *          summary: "Create a new order"
+     *          tags: [Orders]
+     *          requestBody:
+     *              required: true
+     *              content:
+     *                  application/json:
+     *                      schema:
+     *                          $ref: "#/components/schemas/Order"
+     *          responses:
+     *              '200':
+     *                  $ref: "#/components/responses/Created"
+     *              '500':
+     *                  $ref: "#/components/responses/Error"
+     *              '403':
+     *                  $ref: "#/components/responses/Error"
+     *              default:
+     *                  $ref: "#/components/responses/Error"
+     */
+    static async createOrder(req, res) {
+        try {
+            const order = new OrderModel(
+                null,
+                req.body.productId,
+                ORDER_STATUS_PENDING,
+                new Date(),
+                req.body.customerFirstName,
+                req.body.customerLastName,
+                req.body.customerPhone,
+                req.body.customerEmail                 
+            )
+
+            const result = await OrderModel.create(order)
+
+            res.status(200).json({
+                id: result.insertId,
+                message: "order created"
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to create order",
+                errors: [error]
+            })
+        }
     }
     
     /**
